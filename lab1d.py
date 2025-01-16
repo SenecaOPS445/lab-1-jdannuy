e6e9a7e249029c1b7cd79ac6c43ed6fd1e9a3a79
Jhanlyn Brita Dannuy <jdannuy@myseneca.ca>
2025-01-15 19:09:18 -0500

    Add lab1d.py, CheckLab1.py, and laboutput.txt

---
+++ lab1d.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Initialize the integers
+x = 10
+y = 2
+z = 5
+
+# Perform the math operation
+result = x + y * z
+
+# Print the result
+print(f"{x} + {y} * {z} = {result}")
